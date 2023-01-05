68ff44640d22cc0eee49aa54b6a18ad3776a0666
rewrite0w0 <rewrite0w0@gmail.com>
2023-01-05 17:54:34 +0900

    🔨 Modify:  styled 시험 삼아 넣어봄

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.article`
+  display: flex;
+  flex-direction: row;
+
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
 const App = () => {
   return (
-    <div>
-      <span>asdad</span>
-    </div>
+    <article>
+      <input type="text" />
+      <Wrapper>
+        <section>
+          <header>예정</header>
+          <body>123</body>
+        </section>
+        <section>
+          <header>진행중</header>
+          <main>123</main>
+        </section>
+        <section>
+          <header>완료</header>
+          <main>q</main>
+        </section>
+      </Wrapper>
+    </article>
   );
 };
 
